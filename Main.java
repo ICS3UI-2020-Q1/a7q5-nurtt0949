d2ccd35f33aa2f6ab9a5402a75aea5696eef1fad
nurtt0949 <nurtt0949@wrdsb.ca>
2020-10-13 20:15:19 +0000

    Tells user their first digit

--- Main.java
+++ Main.java
@@ -1,15 +1,32 @@
+
 /**
- *
- * @author 
+ *Tells user their first digit
+ * @author Tolga Nurtekin 
  */
 public class Main {
+
+  public static int firstDigit(int userNum){
+    while(userNum > 10 || userNum < -10){
+      userNum = userNum/10;
+    }
+    return userNum;
+  }
+
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    
+    
+    
+    
+    //create total variable
+    int total = Math.abs(firstDigit(-35));
+    //tell user their first first digit
+    System.out.println(total);
+
     
   }
 }
